40034c51d52a5acb7c8f85e70fc9f73d6d40a4a3
Brady Wetherington <bwetherington@grokability.com>
2021-06-29 13:43:38 -0700

    This is a squashed branch of all of the various commits that make up the new HasCustomFields trait. This should allow us to add custom fields to just about anything we want to within Snipe-IT.
    Below are the commits that have been squashed together:
    
    Initial decoupling of custom field behavior from Assets for re-use
    
    Add new DB columns to Custom Fields and fieldsets for 'type'
    
    WIP: trying to figure out UI for custom fields for things other than Assets, find problematic places
    
    Real progress towards getting to where this stuff might actually work...
    
    Fix the table-name determining code for Custom Fields
    
    Getting it closer to where Assets at least work
    
    Rename the trait to it's new, even better name
    
    Solid progress on the new Trait!
    
    WIP: HasCustomFields, still working some stuff out
    
    Got some basics working; creating custom fields and stuff
    
    HasCustomFields now validates and saves
    
    Starting to yank the other boilerplate code as things start to work (!)
    
    Got the start of defaultValuesForCustomField() working
    
    More progress (squash me!)
    
    Add migrations for default_values_for_custom_fields table
    
    WIP: more towards hasCustomFields trait
    
    Progress cleaning up the PR, fixing FIXME's
    
    New, passing HasCustomFieldsTrait test!
    
    Fix date formatter helper for custom fields
    
    Fixed more FIXME's

--- app/Http/Controllers/AssetModelsController.php
+++ app/Http/Controllers/AssetModelsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Helpers\Helper;
 use App\Http\Requests\ImageUploadRequest;
 use App\Models\AssetModel;
+use App\Models\DefaultValuesForCustomFields;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
@@ -161,7 +162,7 @@ class AssetModelsController extends Controller
         $model->notes = $request->input('notes');
         $model->requestable = $request->input('requestable', '0');
 
-        $this->removeCustomFieldsDefaultValues($model);
+        DefaultValuesForCustomFields::forPivot($model, Asset::class)->delete();
 
         if ($request->input('fieldset_id') == '') {
             $model->fieldset_id = null;
@@ -459,7 +460,7 @@ class AssetModelsController extends Controller
     }
 
     /**
-     * Adds default values to a model (as long as they are truthy)
+     * Adds default values to a model (as long as they are truthy) (does this mean I cannot set a default value of 0?)
      *
      * @param  AssetModel $model
      * @param  array      $defaultValues
@@ -494,22 +495,12 @@ class AssetModelsController extends Controller
         }
 
         foreach ($defaultValues as $customFieldId => $defaultValue) {
-            if(is_array($defaultValue)){
-                $model->defaultValues()->attach($customFieldId, ['default_value' => implode(', ', $defaultValue)]);
-            }elseif ($defaultValue) {
-                $model->defaultValues()->attach($customFieldId, ['default_value' => $defaultValue]);
+            if(is_array($defaultValue)) {
+                $defaultValue = implode(', ', $defaultValue);
             }
+            DefaultValuesForCustomFields::updateOrCreate(['custom_field_id' => $customFieldId,'item_pivot_id' => $model->id], ['default_value' => $defaultValue]);
         }
         return true;
     }
 
-    /**
-     * Removes all default values
-     *
-     * @return void
-     */
-    private function removeCustomFieldsDefaultValues(AssetModel $model)
-    {
-        $model->defaultValues()->detach();
-    }
 }
